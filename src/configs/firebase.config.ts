b4e10da2cfa9965be90403a08710ced7c01dc6b5
Hưng Đoàn Đại <daihung.dev@gmail.com>
2026-03-02 22:48:05 +0700

    fix: load firebase from env

--- src/configs/firebase.config.ts
+++ src/configs/firebase.config.ts
@@ -1,14 +1,17 @@
 import admin from "firebase-admin";
-import path from "path";
 
-const serviceAccount = require(path.resolve("firebase-service-account.json"));
-
-if (!admin.apps.length) {
-  admin.initializeApp({
-    credential: admin.credential.cert(serviceAccount),
-    storageBucket: "event-booking-617bf.firebasestorage.app",
-  });
+if (!process.env.FIREBASE_SERVICE_ACCOUNT_BASE64) {
+  throw new Error("Missing FIREBASE_SERVICE_ACCOUNT_BASE64");
 }
 
-export const bucket = admin.storage().bucket();
+const serviceAccount = JSON.parse(
+  Buffer.from(process.env.FIREBASE_SERVICE_ACCOUNT_BASE64, "base64").toString(
+    "utf-8",
+  ),
+);
+
+admin.initializeApp({
+  credential: admin.credential.cert(serviceAccount),
+});
+
 export default admin;
