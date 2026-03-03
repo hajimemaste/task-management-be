ff252dddad251daa3ec788fd9a36a0346652c899
Hưng Đoàn Đại <daihung.dev@gmail.com>
2026-03-03 14:21:46 +0700

    update

--- src/services/auth.service.ts
+++ src/services/auth.service.ts
@@ -392,7 +392,7 @@ export const forgotPasswordService = async (email: string) => {
 
   await user.save();
 
-  await sendOTPEmail(email, otp);
+  await sendOTPEmail("daihung.dev@gmail.com", otp);
 
   return {
     message: "Đã gửi OTP reset mật khẩu",
