8c72bc36b3867e99a8449b25583cfbbce35dd3e6
Hưng Đoàn Đại <daihung.dev@gmail.com>
2026-04-08 03:00:30 +0700

    updatre

--- src/configs/firebase.config.ts
+++ src/configs/firebase.config.ts
@@ -1,9 +1,23 @@
 import admin from "firebase-admin";
-import serviceAccount from "../../serviceAccountKey.json";
 
+if (!process.env.FIREBASE_SERVICE_ACCOUNT_BASE64) {
+  throw new Error("Missing FIREBASE_SERVICE_ACCOUNT_BASE64");
+}
+
+const serviceAccount = JSON.parse(
+  Buffer.from(process.env.FIREBASE_SERVICE_ACCOUNT_BASE64, "base64").toString(
+    "utf-8",
+  ),
+);
+
+// 🔥 FIX 1: sửa private key
+serviceAccount.private_key = serviceAccount.private_key.replace(/\\n/g, "\n");
+
+// 🔥 FIX 2: chỉ init 1 lần
 if (!admin.apps.length) {
   admin.initializeApp({
-    credential: admin.credential.cert(serviceAccount as admin.ServiceAccount),
+    credential: admin.credential.cert(serviceAccount),
+    storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
   });
 }
 
