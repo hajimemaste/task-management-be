3dbc94ed5f7edef528ace4a1f4f3b849bd9c11d0
Hưng Đoàn Đại <daihung.dev@gmail.com>
2026-04-08 02:51:02 +0700

    update

--- src/configs/firebase.config.ts
+++ src/configs/firebase.config.ts
@@ -1,23 +1,11 @@
 import admin from "firebase-admin";
+import serviceAccount from "../../serviceAccountKey.json";
 
-if (!process.env.FIREBASE_SERVICE_ACCOUNT_BASE64) {
-  throw new Error("Missing FIREBASE_SERVICE_ACCOUNT_BASE64");
-}
-
-const serviceAccount = JSON.parse(
-  Buffer.from(process.env.FIREBASE_SERVICE_ACCOUNT_BASE64, "base64").toString(
-    "utf-8",
-  ),
-);
-
-// 🔥 FIX key
-serviceAccount.private_key = serviceAccount.private_key.replace(/\\n/g, "\n");
-
-// 🔥 QUAN TRỌNG: chỉ init 1 lần
 if (!admin.apps.length) {
   admin.initializeApp({
-    credential: admin.credential.cert(serviceAccount),
+    credential: admin.credential.cert(serviceAccount as admin.ServiceAccount),
   });
 }
 
+export const bucket = admin.storage().bucket();
 export default admin;
